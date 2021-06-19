0ae7007ebf9dccbeea57f8e4aeea47bb281666bd
benjaminjulian <57944255+benjaminjulian@users.noreply.github.com>
2021-06-19 00:10:40 +0000

    Update hita-raka.ino
    Skýringar

--- hita-raka.ino
+++ hita-raka.ino
@@ -1,12 +1,16 @@
 #include "DHT.h"
+
+// þessi include má fjarlægja ef bara hita-raka-mælirinn er notaður:
 #include <Wire.h>
 #include "Adafruit_SGP30.h"
 #include <SPI.h>
 #include <Adafruit_BMP280.h>
 
-Adafruit_SGP30 sgp;
 #define DHTPIN A1
 #define DHTTYPE DHT11
+
+// næstu þrjár línur mega fjúka ef bara hita-raka-mælirinn er notaður:
+Adafruit_SGP30 sgp;
 Adafruit_BMP280 bmp; // use I2C interface
 Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
 
@@ -16,6 +20,8 @@ void setup() {
   Serial.begin(9600);
 
   dht.begin();
+  
+  // Afgangurinn af fallinu má fara ef bara hita-raka-mælirinn er notaður:
   bmp.begin();
   sgp.begin();
   
@@ -27,16 +33,10 @@ void setup() {
 }
 
 void loop() {
-  sensors_event_t temp_event, pressure_event;
-  bmp_pressure->getEvent(&pressure_event);
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
   if (isnan(h) || isnan(t)) {
-    return;
-  }
-  if (! sgp.IAQmeasure()) {
-    Serial.println("villa í skynjara");
     return;
   }
 
@@ -44,6 +44,16 @@ void loop() {
   Serial.println(h);
   Serial.print("hiti:");
   Serial.println(t);
+  
+  // Allt fram að delay fallinu má fara ef bara hita-raka-mælirinn er notaður:
+  
+  sensors_event_t temp_event, pressure_event;
+  bmp_pressure->getEvent(&pressure_event);
+  
+  if (! sgp.IAQmeasure()) {
+    Serial.println("villa í skynjara");
+    return;
+  }
   Serial.print("eco2:");
   Serial.println(sgp.eCO2);
   Serial.print("hpa:");
